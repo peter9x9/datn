c04f09b028b355f0f3e4f78aac030573587d3da0
peter9x9 <dinhmanh162001@gmail.com>
2024-04-18 01:31:25 +0700

    project ban dau

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DoAnTotNghiep.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool IsLoaded= false;
+        public ICommand LoadedWindowCommand { get; set; }
+        public MainViewModel() {
+            LoadedWindowCommand = new RelayCommand<object>((p) => { return true; },
+                (p) => {
+                    IsLoaded = true;
+                    LoginWindow loginWindow = new LoginWindow();
+                    loginWindow.ShowDialog();
+                });
+            
+        }  
+    }
+}
